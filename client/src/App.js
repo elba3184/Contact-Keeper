ec7b18dd631ee8df6f64325c1e40a330360bbe63
Elba Chimilio <elbachimilio@gmail.com>
2019-12-03 13:38:37 -0500

    Implemented Contact Context and added Contact and ContactItem components

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar.js';
 import Home from './components/pages/Home.js';
 import About from './components/pages/About.js';
+import ContactState from './context/contact/ContactState.js';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts.js';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <h1>Home</h1>
+      <div>{/* {contact form} */}</div>
+      <div>
+        <Contacts />
+      </div>
     </div>
   );
 };
