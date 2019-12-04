d27f938350378fc7394ac3ec59af93331103da86
Elba Chimilio <elbachimilio@gmail.com>
2019-12-04 14:37:29 -0500

    Added Contact Form and ability to Create and Delete contact - not hooked to DB yet

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
 
   return (
     <div className='card bg-light'>
@@ -31,13 +38,15 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <button className='btn btn-dark btn-sm'>Edit</button>
-      <button className='btn btn-danger btn-sm'>Delete</button>
+      <button className='btn btn-danger btn-sm' onClick={onDelete}>
+        Delete
+      </button>
     </div>
   );
 };
 
 ContactItem.propTypes = {
-    contact: PropTypes.object.isRequired,
-}
+  contact: PropTypes.object.isRequired,
+};
 
 export default ContactItem;
