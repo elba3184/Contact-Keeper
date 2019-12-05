b42e7c6c9df8d13ff71aa05ce8e2cc4e380193d0
Elba Chimilio <elbachimilio@gmail.com>
2019-12-05 08:54:50 -0500

    Completed register and alerts on register

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,36 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar.js';
 import Home from './components/pages/Home.js';
 import About from './components/pages/About.js';
+import Register from './components/auth/Register.js';
+import Login from './components/auth/Login.js';
+import Alerts from './components/layout/Alerts.js';
 import ContactState from './context/contact/ContactState.js';
+import AuthState from './context/auth/AuthState.js';
+import AlertState from './context/alert/AlertState.js';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
