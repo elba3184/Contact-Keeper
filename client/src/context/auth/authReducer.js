07ed8f70b5671c5cd97b3336c3365e2aae897a5c
Elba Chimilio <elbachimilio@gmail.com>
2020-06-25 10:56:37 -0400

    Added token authentication to registration

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,6 +11,13 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -20,11 +27,12 @@ export default (state, action) => {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
-        isAuthenticated: true,
+        isAuthenticated: false,
         loading: false,
         user: null,
         error: action.payload,
